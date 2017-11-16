0252a09e2d4db07c9158aa3b61cc15eabd179ab5
Apoorva Iyer <apoorva.iyer@gmail.com>
2017-11-16 08:48:06 -0800

    LOLT Website Original

---
+++ main.js
@@ -0,0 +1,8 @@
+function showpopup(intro) {
+    var begin = 'popup' + intro;
+    document.getElementById(begin).style.display = "block";
+}
+function hidepopup(intro) {
+    var begin = 'popup' + intro;
+    document.getElementById(begin).style.display = "none";
+}
